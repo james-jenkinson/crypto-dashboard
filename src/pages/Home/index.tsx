54fe025df18e2d4bc72eeeaf35f77c69ea182831
James Jenkinson <j.a.jenkinson@gmx.com>
2021-03-10 16:28:09 +0100

    Layout home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,17 +5,22 @@ import LoadingSpinner from '../../components/LoadingIndicator'
 import MarketPlot from '../../components/MarketPlot'
 import PastSearches from '../../components/PastSearches'
 import { coinContext } from '../../data/CoinContext'
+import './home.css'
 
 function Home(): JSX.Element {
   const { isLoading } = useContext(coinContext)
 
   return (
-    <div>
-      <FetchCurrency />
-      <PastSearches />
-      <LoadingSpinner isLoading={isLoading} />
-      <CoinDetails />
-      <MarketPlot />
+    <div className="home">
+      <div className="home__fetch-currency-form">
+        <FetchCurrency />
+        <PastSearches />
+      </div>
+      <div className="home__coin-details">
+        <LoadingSpinner isLoading={isLoading} />
+        <CoinDetails />
+        <MarketPlot />
+      </div>
     </div>
   )
 }
